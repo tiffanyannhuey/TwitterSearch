32c5db353db3b20c1ffac3dc37001b80b01e0efc
tiffanyannhuey <87tiffanyhuey@gmail.com>
2019-03-05 13:54:28 -0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Twitter';
+  title = 'Twitter Search';
+  searchquery = '';
+  tweetsdata;
+
+  constructor(private http: HttpClient) { }
+
+  makecall() {
+    var headers = new HttpHeaders();
+    
+    headers.append('Content-Type', 'application/X-www-form-urlencoded');
+    
+    this.http.post('http://localhost:4200/authorize', {headers: headers}).subscribe((res) => {
+      console.log(res);
+    })
+  }
+  
 }
