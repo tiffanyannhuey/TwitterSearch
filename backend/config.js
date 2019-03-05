c8cc006c29715159a2db630799c81f97b66c7af7
tiffanyannhuey <87tiffanyhuey@gmail.com>
2019-03-05 14:06:17 -0800

    add api keys

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+var appsettings = {
+    consumerkey: '1srdNZRFtNYq4uyeNLWhnDyjE',
+    consumersecret: 'YoMW8LIn5JRVx85VULSHn0qzLMJq0s3SDj7B2Mt3WXZb8Nl0sk',
+    bearertoken: ''
+};
+
+module.exports = appsettings;
